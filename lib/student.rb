d039c6c83da16e41cd968a55d2348cd239eda349
jamieberrier <berrier.jamie@gmail.com>
2019-06-03 18:51:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,8 +18,10 @@ class Student
       FROM students
       WHERE grade = "9"
     SQL
-    
 
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end 
   end
 
   def self.all
