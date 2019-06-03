c133346d33b93b7022a53eb27db614eb9e21672b
jamieberrier <berrier.jamie@gmail.com>
2019-06-03 19:27:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -67,9 +67,9 @@ class Student
       FROM students
     SQL
 
-    DB[:conn].execute(sql).map do |row|
+    array = DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
-    end.select! { |student| student.grade == x }
+    end#.select! { |student| student.grade == x }
   end
 
   def self.all
