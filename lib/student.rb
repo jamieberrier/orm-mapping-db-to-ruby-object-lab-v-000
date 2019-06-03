9c6e4e0a95478547f680309863ccf2e1fb2a4ecd
jamieberrier <berrier.jamie@gmail.com>
2019-06-03 19:41:23 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -64,7 +64,7 @@ class Student
   def self.all_students_in_grade_X(x)
     array = self.all
     #binding.pry
-    array.select { |student| student.grade == "x" }
+    array.select { |student| student.grade == 'x' }
   end
 
   def self.all
